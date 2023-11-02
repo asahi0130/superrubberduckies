6447111a7b6c6269034cba710011c665e9eed195
asahi0130 <asahi.iphone@gmail.com>
2023-11-02 14:16:33 +0900

    main

--- pages/university/[id].tsx
+++ pages/university/[id].tsx
@@ -2,30 +2,7 @@ import { GetServerSideProps } from 'next';
 import fs from 'fs';
 import path from 'path';
 
-export const getServerSideProps: GetServerSideProps = async ({ params }) => {
-  // Get the slug from the URL
-  const slug = params?.id;
 
-  // Read the university.json file from the public folder
-  const filePath = path.join(process.cwd(), 'public', 'university.json');
-  const jsonData = fs.readFileSync(filePath, 'utf8');
-  const universities = JSON.parse(jsonData);
-
-  // Find the university by slug
-  const university = universities.find((u: any) => u.slug === slug);
-
-  // If the university is not found, return notFound: true
-  if (!university) {
-    return {
-      notFound: true,
-    };
-  }
-
-  // Return the university data as props
-  return {
-    props: { university },
-  };
-};
 
 const UniversityPage = ({ university }) => {
   // Render your page with the university data
@@ -41,3 +18,29 @@ const UniversityPage = ({ university }) => {
 };
 
 export default UniversityPage;
+
+
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+    // Get the slug from the URL
+    const slug = params?.id;
+  
+    // Read the university.json file from the public folder
+    const filePath = path.join(process.cwd(), 'public', 'university.json');
+    const jsonData = fs.readFileSync(filePath, 'utf8');
+    const universities = JSON.parse(jsonData);
+  
+    // Find the university by slug
+    const university = universities.find((u: any) => u.slug === slug);
+  
+    // If the university is not found, return notFound: true
+    if (!university) {
+      return {
+        notFound: true,
+      };
+    }
+  
+    // Return the university data as props
+    return {
+      props: { university },
+    };
+  };
