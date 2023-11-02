59e4ccd7dbfca8bf90d30c8a24156df9b0010b2c
asahi0130 <asahi.iphone@gmail.com>
2023-11-02 14:15:58 +0900

    main

---
+++ pages/university/[id].tsx
@@ -0,0 +1,43 @@
+import { GetServerSideProps } from 'next';
+import fs from 'fs';
+import path from 'path';
+
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+  // Get the slug from the URL
+  const slug = params?.id;
+
+  // Read the university.json file from the public folder
+  const filePath = path.join(process.cwd(), 'public', 'university.json');
+  const jsonData = fs.readFileSync(filePath, 'utf8');
+  const universities = JSON.parse(jsonData);
+
+  // Find the university by slug
+  const university = universities.find((u: any) => u.slug === slug);
+
+  // If the university is not found, return notFound: true
+  if (!university) {
+    return {
+      notFound: true,
+    };
+  }
+
+  // Return the university data as props
+  return {
+    props: { university },
+  };
+};
+
+const UniversityPage = ({ university }) => {
+  // Render your page with the university data
+  // ...
+
+  return (
+    <>
+    <section id="top">
+<h1 className='text-xl text-center my-8' >{university.title}</h1>
+    </section>
+    </>
+  );
+};
+
+export default UniversityPage;
