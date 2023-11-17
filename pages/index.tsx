40400c60ff49ebb408c742465fd193f8d728d978
asahi0130 <asahi.iphone@gmail.com>
2023-11-17 12:38:19 +0900

    main

--- pages/index.tsx
+++ pages/index.tsx
@@ -61,9 +61,44 @@ function Home() {
 
 </div>
 </div>
-
 </section>
 
+<hr className="h-px my-16 bg-gray-300 mx-16 border-0" />
+
+
+<section id="choice" className="text-white mb-32 ">
+  <div className="pearl mx-16 md:mx-36 lg:mx-72 rounded-2xl  pt-4 pb-12">
+
+<h2 className="text-3xl text-center my-4">Choose By</h2>
+
+<div className="textColor  flex flex-row gap-x-4 text-center justify-between mx-4 md:mx-12 mb-16">
+<button className="bg-gray-200 text-sm md:text-xl py-2 hover:bg-gray-300 active:opacity-80 w-full rounded-lg">City Life</button>
+<button className="bg-gray-200 text-sm md:text-xl py-2 hover:bg-gray-300 active:opacity-80 w-full rounded-lg">Research</button>
+<button className="bg-gray-200 text-sm md:text-xl py-2 hover:bg-gray-300 active:opacity-80 w-full rounded-lg">Research</button>
+</div>
+
+
+<div className="textColor grid grid-col-1 md:grid-cols-2 gap-y-8 gap-x-4 md:gap-x-12 mx-4 md:mx-12 lg:mx-24">
+<div className="bg-white text-center rounded-xl px-4 py-12 text-2xl">
+  GIGA
+</div>
+<div className="bg-white text-center rounded-xl px-4 py-12 text-2xl">
+  SILS
+</div>
+<div className="bg-white text-center rounded-xl px-4 py-12 text-2xl">
+  PEARL
+</div>
+<div className="bg-white text-center rounded-xl px-4 py-12 text-2xl">
+  PSE
+</div>
+
+
+
+</div>
+
+  </div>
+
+</section>
     
 
 
