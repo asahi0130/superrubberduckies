ae90e2a7bf068fba55c8f0cbbdfe08603798369f
asahi0130 <asahi.iphone@gmail.com>
2023-11-30 14:13:31 +0900

    main

--- components/UniversityCard.js
+++ components/UniversityCard.js
@@ -20,16 +20,13 @@ function UniversityCard({b, index}){
 {/*  */}
   <div className={`relative ${b.slug}  shadow-xl rounded-lg p-4 h-[200px] w-7/8 m-auto hover:opacity-80  transition-all`}> 
 
-    {/* <div className="border-gray-400 border-2 block relative h-48 rounded overflow-hidden">
-      <img
-      layout="fill" alt={b.title} className="object-cover object-center w-full h-full block" src={b.image}  />
-    </div> */}
-
-    <div className="mt-4">
-    <p className=" flex justify-center  title-font text-2xl font-medium text-white">{b.title}</p>
 
 
+<div className='h-full flex flex-col justify-center items-center  title-font text-2xl font-medium text-white'>
+    <p className="">{b.schoolshort}</p>
+    <p className="">{b.program}</p>
     </div>
+
 
   </div>
   </Link>
