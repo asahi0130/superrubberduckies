1685f79b6579e9924dad14cba436722afa42cd28
asahi0130 <asahi.iphone@gmail.com>
2023-11-30 13:31:12 +0900

    main

--- pages/quiz.tsx
+++ pages/quiz.tsx
@@ -56,7 +56,7 @@ function checkCompleted(){
     <div className="relative bg-[#d9d9d9] rounded-lg mx-16 md:mx-36 lg:mx-72 textColor pt-16 pb-8">
       <h2 className="text-6xl absolute z-40 -top-4 ml-4 font-extrabold">1</h2>
 <span className="absolute top-2 text-4xl">____</span>
-      <p className="ml-8 text-lg">Are you more of a city person or out-skirt person?</p>
+      <p className="mx-8 text-lg">Are you more of a city person or out-skirt person?</p>
       <div className="mt-4 flex justify-center gap-x-8">
       <button onClick={() => setAnswer1(true)} className={` ${answer1 == true ? ('bg-[#e7ecff]') : ('bg-white')} textColor py-4 px-10 text-lg hover:opacity-80 active:opacity-70`}>Yes</button>
       <button onClick={() => setAnswer1(false)} className={` ${answer1 == false ? ('bg-[#e7ecff]') : ('bg-white')} textColor py-4 px-10 text-lg hover:opacity-80 active:opacity-70`}>No</button>
@@ -68,7 +68,7 @@ function checkCompleted(){
     <div className="relative bg-[#d9d9d9] rounded-lg mx-16 md:mx-36 lg:mx-72 textColor pt-16 pb-8">
       <h2 className="text-6xl absolute z-40 -top-4 ml-4 font-extrabold">2</h2>
 <span className="absolute top-2 text-4xl">____</span>
-      <p className="ml-8 text-lg">How international do you want to be in university?</p>
+      <p className="mx-8 text-lg">How international do you want to be in university?</p>
       <div className="mt-4 flex justify-center gap-x-8">
       <button onClick={() => setAnswer2(true)} className={` ${answer2 == true ? ('bg-[#e7ecff]') : ('bg-white')} textColor py-4 px-10 text-lg hover:opacity-80 active:opacity-70`}>Yes</button>
       <button onClick={() => setAnswer2(false)} className={` ${answer2 == false ? ('bg-[#e7ecff]') : ('bg-white')} textColor py-4 px-10 text-lg hover:opacity-80 active:opacity-70`}>No</button>
@@ -80,7 +80,7 @@ function checkCompleted(){
     <div className="relative bg-[#d9d9d9] rounded-lg mx-16 md:mx-36 lg:mx-72 textColor pt-16 pb-8">
       <h2 className="text-6xl absolute z-40 -top-4 ml-4 font-extrabold">3</h2>
 <span className="absolute top-2 text-4xl">____</span>
-      <p className="ml-8 text-lg">Have you decided on specific major already?</p>
+      <p className="mx-8 text-lg">Have you decided on specific major already?</p>
       <div className="mt-4 flex justify-center gap-x-8">
       <button onClick={() => setAnswer3(true)} className={` ${answer3 == true ? ('bg-[#e7ecff]') : ('bg-white')} textColor py-4 px-10 text-lg hover:opacity-80 active:opacity-70`}>Yes</button>
       <button onClick={() => setAnswer3(false)} className={` ${answer3 == false ? ('bg-[#e7ecff]') : ('bg-white')} textColor py-4 px-10 text-lg hover:opacity-80 active:opacity-70`}>No</button>
@@ -92,7 +92,7 @@ function checkCompleted(){
     <div className="relative bg-[#d9d9d9] rounded-lg mx-16 md:mx-36 lg:mx-72 textColor pt-16 pb-8">
       <h2 className="text-6xl absolute z-40 -top-4 ml-4 font-extrabold">4</h2>
 <span className="absolute top-2 text-4xl">____</span>
-      <p className="ml-8 text-lg">Do you rather go into the city or chill on a beach?</p>
+      <p className="mx-8 text-lg">Do you rather go into the city or chill on a beach?</p>
       <div className="mt-4 flex justify-center gap-x-8">
       <button onClick={() => setAnswer4(true)} className={` ${answer4 == true ? ('bg-[#e7ecff]') : ('bg-white')} textColor py-4 px-10 text-lg hover:opacity-80 active:opacity-70`}>Yes</button>
       <button onClick={() => setAnswer4(false)} className={` ${answer4 == false ? ('bg-[#e7ecff]') : ('bg-white')} textColor py-4 px-10 text-lg hover:opacity-80 active:opacity-70`}>No</button>
@@ -105,7 +105,7 @@ function checkCompleted(){
     <div className="relative bg-[#d9d9d9] rounded-lg mx-16 md:mx-36 lg:mx-72 textColor pt-16 pb-8">
       <h2 className="text-6xl absolute z-40 -top-4 ml-4 font-extrabold">5</h2>
 <span className="absolute top-2 text-4xl">____</span>
-      <p className="ml-8 text-lg">Do you want to study abroad?</p>
+      <p className="mx-8 text-lg">Do you want to study abroad?</p>
       <div className="mt-4 flex justify-center gap-x-8">
       <button onClick={() => setAnswer5(true)} className={` ${answer5 == true ? ('bg-[#e7ecff]') : ('bg-white')} textColor py-4 px-10 text-lg hover:opacity-80 active:opacity-70`}>Yes</button>
       <button onClick={() => setAnswer5(false)} className={` ${answer5 == false ? ('bg-[#e7ecff]') : ('bg-white')} textColor py-4 px-10 text-lg hover:opacity-80 active:opacity-70`}>No</button>
@@ -121,7 +121,7 @@ function checkCompleted(){
     <div className="relative bg-[#d9d9d9] rounded-lg mx-16 md:mx-36 lg:mx-72 textColor pt-16 pb-8">
       <h2 className="text-7xl absolute z-40 -top-8 ml-4 font-extrabold left-1/2 transform -translate-x-1/2">Results</h2>
 <span className="absolute top-2 text-4xl left-1/2 transform -translate-x-1/2">_____</span>
-      <p className="ml-8 text-lg">{checkCompleted() ? ('Quiz completed') : ('Please finish the quiz')}</p>
+      <p className="mx-8 text-lg">{checkCompleted() ? ('Quiz completed') : ('Please finish the quiz')}</p>
 
     </div>
   </section>
