f8ddea09326ca2856b8d7d66c5433f6ec4b49e55
asahi0130 <asahi.iphone@gmail.com>
2024-01-11 14:29:39 +0900

    main

--- pages/index.tsx
+++ pages/index.tsx
@@ -50,7 +50,7 @@ function Home() {
 
 <section className="mb-56" id="top">
 
-<h1 className="md:text-8xl text-5xl text-left ml-8 md:ml-32  robotoFont textColor font-bold lg:mb-72 md:mt-24 md:mb-32 mb-10 lg:w-7/12">English College Picker</h1>
+<h1 className="md:text-8xl text-5xl text-left ml-8 md:ml-32  robotoFont textColor font-bold lg:mb-72 md:mt-24 md:mb-32 mb-32 lg:w-7/12">English College Picker</h1>
 
 <div className="flex flex-row relative">
 <img className="absolute -left-16 md:bottom-48 " height={128} width={128} src="/images/keio.png" />
